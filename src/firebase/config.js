086ad512e5e91110b9ff9a2ecaa1e7e67be32492
Adnan Bacic <adnan-bacic@hotmail.com>
2023-02-28 17:16:12 +0100

    Update config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDUxfJ3_Zbkbt25v5yliiwlZGZtFXQQbTQ",
+    authDomain: "vue-3-firebase-9-reading-list.firebaseapp.com",
+    projectId: "vue-3-firebase-9-reading-list",
+    storageBucket: "vue-3-firebase-9-reading-list.appspot.com",
+    messagingSenderId: "906312027929",
+    appId: "1:906312027929:web:12caf1efadd71088723f88"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+//firestore
+const db = getFirestore();
+
+export {
+    db
+}
